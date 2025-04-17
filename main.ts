8fe4036b9853fb10a9b902db4ede75afcf7ce8d7
Thanachote Ngamkana <gbb.tn@outlook.com>
2025-04-17 15:09:51 +0700

    update: Change collection3 order

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ import { merge } from "./merge";
 function main() {
   const collection1: number[] = [];
   const collection2: number[] = [2, 3];
-  const collection3: number[] = [1, 4];
+  const collection3: number[] = [4, 1];
 
   const result = merge(collection1, collection2, collection3);
   console.log(result);
